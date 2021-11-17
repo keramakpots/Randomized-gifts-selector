73867e012340354fabb881500ae54931e2dcf24b
Marek <keramakpots@gmail.com>
2021-11-17 21:24:25 +0100

    Code cleanup

--- src/main/java/com/stopka/giftsrandomizer/match/Matcher.java
+++ src/main/java/com/stopka/giftsrandomizer/match/Matcher.java
@@ -15,30 +15,48 @@ public class Matcher {
         List<Person> sharedGiftsList = new ArrayList<>();
 
         for (Person p : personList) {
-            if (p.getGiving() == null) {
-                List<Person> others = new ArrayList<>();
-                for (Person otherPerson : personList) {
-                    if (isNotSamePerson(p, otherPerson) && otherPerson.getReceiving() == null) {
-                        others.add(otherPerson);
-                    }
-                }
-                Person person;
-                person = others.get(rand.nextInt(others.size()));
-                if (person.getReceiving() == null && isNotSamePerson(p, person) && isNotGivingSameAsReceiving(p, person)) {
-                    p.setGiving(person);
-                    person.setReceiving(p);
-                    if (person.getGiving() != null && person.getReceiving() != null && !sharedGiftsList.contains(person)) {
-                        sharedGiftsList.add(person);
-                    }
-
-                    if (p.getGiving() != null && p.getReceiving() != null && !sharedGiftsList.contains(p)) {
-                        sharedGiftsList.add(p);
-                    }
-                }
-
-            }
+            handlePersonWithoutReceiver(personList, sharedGiftsList, p);
         }
         return sharedGiftsList;
+    }
+
+    private void handlePersonWithoutReceiver(List<Person> personList, List<Person> sharedGiftsList, Person p) {
+        if (p.getGiving() == null) {
+            List<Person> others = getListOfOthers(personList, p);
+            Person person;
+            person = others.get(rand.nextInt(others.size()));
+            assignGift(sharedGiftsList, p, person);
+
+        }
+    }
+
+    private List<Person> getListOfOthers(List<Person> personList, Person p) {
+        List<Person> others = new ArrayList<>();
+        for (Person otherPerson : personList) {
+            if (isNotSamePerson(p, otherPerson) && otherPerson.getReceiving() == null) {
+                others.add(otherPerson);
+            }
+        }
+        return others;
+    }
+
+    private void assignGift(List<Person> sharedGiftsList, Person p, Person person) {
+        if (person.getReceiving() == null && isNotSamePerson(p, person) && isNotGivingSameAsReceiving(p, person)) {
+            assingGiver(p, person);
+            addPersonToResults(sharedGiftsList, person);
+            addPersonToResults(sharedGiftsList, p);
+        }
+    }
+
+    private void addPersonToResults(List<Person> sharedGiftsList, Person person) {
+        if (person.getGiving() != null && person.getReceiving() != null && !sharedGiftsList.contains(person)) {
+            sharedGiftsList.add(person);
+        }
+    }
+
+    private void assingGiver(Person p, Person person) {
+        p.setGiving(person);
+        person.setReceiving(p);
     }
 
     private boolean isNotSamePerson(Person p, Person person) {
